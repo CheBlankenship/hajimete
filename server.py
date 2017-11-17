17d242c3c18fdb53e0e521c34b934a45a667b233
CheBlankenship <cnakamura336@gmail.com>
2017-11-16 18:51:28 -0600

    config .env .gitignore

--- server.py
+++ server.py
@@ -1,28 +1,31 @@
 #!flask/bin/python
-from flask import Flask, render_template, request, redirect, session, flash, jsonify
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+from flask import Flask, render_template, redirect, request, jsonify
 import pg
+#from wiki_linkify import wiki_linkify
 import os
 
-
-
-#DataBase Connection
-app = Flask(__name__)
 db = pg.DB(
-       host='nakamura.c2iqmtffajql.us-east-1.rds.amazonaws.com',
-       user='root',
-       passwd='grandmava',
-       dbname='nakamuradb'
+    dbname=os.environ.get('PG_DBNAME'),
+    host=os.environ.get('PG_HOST'),
+    user=os.environ.get('PG_USERNAME'),
+    passwd=os.environ.get('PG_PASSWORD')
 )
+tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask('nakamura', template_folder=tmp_dir)
 
 @app.route('/')
-def init_page():
-    init_value = "Hola to py server!!!"
-    return jsonify(init_value)
+def home():
+    return redirect('/summary')
 
-@app.route('/initReturn')
-def get_json():
-    all_products = db.query("SELECT * FROM testschema.stores").dictresult()
-    return jsonify(all_products)
+
+@app.route('/summary')
+def summary():
+    queryData = db.query("select * from testschema.stores")
+    resultData = queryData.dictresult()
+    return jsonify(resultData)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
